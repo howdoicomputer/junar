361b3e11c4570ccc75d3c1a625d45b14d668febf
howdoicomputer <tyler@insidevault.com>
2014-08-04 23:32:36 -0700

    a wrapper class for the httparty class

---
+++ lib/sjod/client.rb
@@ -0,0 +1,27 @@
+require 'httparty'
+
+# Anything to do with providing a client object
+#
+# @author Tyler Hampton
+# @since 0.0.1
+module SJOD
+  class Client
+    include HTTParty
+    attr_reader :token
+    attr_reader :uri
+
+    # Initialize a new Client object
+    #
+    # @param [Hash] opts
+    #   @option opts [String] :token Manually load the API token
+    #
+    # @return [SJOD::Client] An API handle object for doing CRUD operations
+    def initialize(opts={})
+      @token = opts[:token]
+      @uri = opts[:uri]
+
+      # Set the base URI for the api at a class level
+      self.class.base_uri @uri
+    end
+  end
+end
