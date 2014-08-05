291b7f047189a6b677cbbfced99a5994403dc49d
howdoicomputer <tyler@insidevault.com>
2014-08-05 00:31:08 -0700

    made tests buttah

--- spec/sjod/client_spec.rb
+++ spec/sjod/client_spec.rb
@@ -1,12 +1,20 @@
 describe SJOD do
-  
-  describe '.initialize' do
-    client = SJOD::Client.new ({
+  before(:each) do
+    @client = SJOD::Client.new ({
       token: 'e60e3ec6c2d32969a95127968f03412f69f9b61c',
       uri: 'http://api.data.sanjoseca.gov'
     })
-
-    it { expect(client.token).to be_a(String) }
   end
 
+  describe '.initialize' do
+    it { expect(@client).to be_a(SJOD::Client) }
+  end
+
+  describe '.token' do
+    it { expect(@client.token).to be_a(String) }
+  end
+
+  describe '.uri' do
+    it { expect(@client.uri). to be_a(String) }
+  end
 end
