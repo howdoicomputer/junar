d28482fe02c804b08e4537cc36b3edd9fff83a7e
Tyler Hampton <tyler@insidevault.com>
2014-09-12 11:10:24 -0700

    ISSUE-#3 added vcr and pp to dependencies and configure them

---
+++ spec/junar/search_spec.rb
@@ -0,0 +1,22 @@
+describe Junar::Client do
+  before(:each) do
+    @client = Junar::Client.new ({
+      token: 'd759b5b71ce13ac5e74bfa466a5caf033a1c5931',
+      uri: 'http://api.data.sanjoseca.gov'
+    })
+  end
+
+  describe '.search' do
+    it 'should return results from a datastreams query', :vcr do
+      result = @client.search category: 'datastreams', query: 'FIRE'
+
+      expect(result).to be_a(Array)
+    end
+
+    it 'should return results from a dashboards query', :vcr do
+      result = @client.search category: 'dashboards', query: 'FIRE'
+
+      expect(result).to be_a(Array)
+    end
+  end
+end
