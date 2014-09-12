6eeeb24cce3a23b2d50d4a4e47f6cef522a876f1
Tyler Hampton <tyler@insidevault.com>
2014-09-12 13:41:07 -0700

    ISSUE-#7 added code climate requirement in spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,9 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require 'vcr'
 
 VCR.configure do |c|
